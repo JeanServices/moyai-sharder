8a7b90d385766abdae8fa1442a0561653ccdce87
Jean Carlos VR <46204992+JeanCarlosVR@users.noreply.github.com>
2021-01-24 14:59:40 -0500

    Update Base.ts

--- structures/Base.ts
+++ structures/Base.ts
@@ -1,17 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Jean Vides. All rights reserved.
+ *  See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import IPC from "./IPC";
+
 export default class Base {
 
-    protected client;
+    protected bot;
     protected clusterID;
     protected ipc;
 
-    public constructor(setup) {
-        this.client = setup.client;
+    public constructor(setup: any) {
+        this.bot = setup.bot;
         this.clusterID = setup.clusterID;
         this.ipc = new IPC();
     }
 
-    public restartCluster(clusterID) {
-        this.ipc.sendTo(clusterID, "restart", { name: "restart" });
+    public restartCluster(clusterID: any) {
+        this.ipc.sendTo(clusterID, "restart", { _eventName: "restart" });
     }
 }
